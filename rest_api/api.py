a1ef924414eb7857c6ece0c20a86c5528e6177f0
Pietro <pietro.pf@gmail.com>
2020-03-23 02:24:16 -0300

    Improved twitter request routine and implemented REST API

---
+++ rest_api/api.py
@@ -0,0 +1,71 @@
+import pymysql.cursors
+import flask
+
+app = flask.Flask(__name__)
+
+class DBManager():
+	def __init__ (self):
+		self.conn = pymysql.connect(host='remotemysql.com',
+							 port=3306,
+							 user='user',
+							 password='pass',
+							 db='db',
+							 charset='utf8mb4',
+							 cursorclass=pymysql.cursors.DictCursor)
+		self.cursor = self.conn.cursor()
+
+	def db_query(self, query):
+		self.cursor.execute(query)
+		return self.cursor.fetchall()
+	
+	def __del__(self):
+		self.cursor.close()
+		self.conn.close()
+
+
+def top_followers():
+	db = DBManager()
+	response = db.db_query("SELECT DISTINCT user, MAX(followers) AS 'followers' FROM tweets GROUP BY user ORDER BY MAX(followers) DESC LIMIT 5")
+	del db
+	return response
+
+def total_posts():
+	db = DBManager()
+	response = db.db_query("""SELECT HOUR(`date`) AS 'hour' , DAY(`date`) AS 'day', MONTH(`DATE`) AS 'month', COUNT(*) AS 'posts' FROM tweets GROUP BY HOUR(`date`),
+							DAY(`date`),MONTH(`DATE`) ORDER BY DAY(`date`) ASC, HOUR(`date`) ASC, MONTH(`DATE`) ASC""")
+	del db
+	return response
+
+def posts_language():
+	db = DBManager()
+	response = db.db_query("SELECT hashtag, language, COUNT(*) AS 'posts' FROM tweets GROUP BY hashtag, language")
+	del db
+	return response
+
+
+def send_response(responseObj):
+	response = flask.jsonify(responseObj)
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	response.headers.add('Access-Control-Allow-Methods', 'GET')
+	response.headers.add('Access-Control-Allow-Headers', 'accept,content-type,Origin,X-Requested-With,Content-Type,access_token,Accept,Authorization,source')
+	response.headers.add('Access-Control-Allow-Credentials', True)
+	return response
+
+
+@app.route("/get_info", methods=["GET"])
+def json_output():
+	requested = flask.request.args.get('id')
+	print(requested)
+	if requested is None:
+		return send_response({"error": {"reason": "invalidParameter", "message": "No parameter was given", "code": 400}})
+	elif requested == "1":
+		return send_response(top_followers())
+	elif requested == "2": 
+		return send_response(total_posts())
+	elif requested == "3": 
+		return send_response(posts_language())
+	else:
+		return send_response({"error": {"reason": "invalidParameter", "message": "No parameter was given", "code": 400}})
+
+if __name__ == '__main__':
+	app.run(host='localhost')
--- twitter_request/main.py
+++ twitter_request/main.py
@@ -13,46 +13,46 @@ api = tweepy.API(auth)
 
 # Hashtags to be stored
 tags = ["openbanking", "apifirst", "devops", "devops", "microservices", "apigateway",
-        "oauth", "swagger", "raml", "openapis"]
+		"oauth", "swagger", "raml", "openapis"]
 
 class DBManager():
-    def __init__ (self):
-        self.conn = pymysql.connect(host='remotemysql.com',
-                             port=3306,
-                             user='user',
-                             password='pass',
-                             db='user',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-        self.cursor = self.conn.cursor()
+	def __init__ (self):
+		self.conn = pymysql.connect(host='remotemysql.com',
+							 port=3306,
+							 user='user',
+							 password='pass',
+							 db='db',
+							 charset='utf8mb4',
+							 cursorclass=pymysql.cursors.DictCursor)
+		self.cursor = self.conn.cursor()
 
-    def add_update(self, query):
-        self.cursor.execute(query)
-        self.conn.commit()
-    
-    def __del__(self):
-        self.cursor.close()
-        self.conn.close()
+	def db_query(self, query):
+		self.cursor.execute(query)
+		self.conn.commit()
+	
+	def __del__(self):
+		self.cursor.close()
+		self.conn.close()
 
 
 def main():
-    db = DBManager()
-    db.add_update("DELETE FROM tweets")
-    for tag in tqdm(tags, position=0):
-        c = tweepy.Cursor(api.search, q=f"(#{tag})", result_type="recent")
+	db = DBManager()
+	db.db_query("DELETE FROM tweets")
+	for tag in tqdm(tags, position=0):
+		c = tweepy.Cursor(api.search, q=f"(#{tag})", result_type="recent")
 
-        for tweet in tqdm(c.items(100), position=1, total=100, leave=False, miniters=1, desc=f"searching {tag}"):
-            text = tweet.text
-            text = text.replace("'", '"')
-            date = str(tweet.created_at)
-            user = tweet.author.screen_name
-            location = tweet.author.location
-            followers = tweet.author.followers_count
-            lang = tweet.lang
+		for tweet in tqdm(c.items(100), position=1, total=100, leave=False, miniters=1, desc=f"searching {tag}"):
+			text = tweet.text
+			text = text.replace("'", '"')
+			date = str(tweet.created_at)
+			user = tweet.author.screen_name
+			location = tweet.author.location
+			followers = tweet.author.followers_count
+			lang = tweet.lang
 
-            query = f"INSERT INTO tweets (hashtag, `text`, `date`, user, location, followers, language) VALUES (\'{tag}\', \'{text}\', \'{date}\', \'{user}\', \'{location}\', {followers}, \'{lang}\');"
-            db.add_update(query)
-
+			query = f"INSERT INTO tweets (hashtag, `text`, `date`, user, location, followers, language) VALUES (\'{tag}\', \'{text}\', \'{date}\', \'{user}\', \'{location}\', {followers}, \'{lang}\');"
+			db.db_query(query)
+	del db
 
 if __name__ == '__main__':
-    main()
+	main()
